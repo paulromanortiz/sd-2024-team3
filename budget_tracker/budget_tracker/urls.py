4f15c5df2cd1ce5dcc0e1b1eec81a2ff94873e25
crackedrooster <giorgioruttell@gmail.com>
2024-10-26 16:39:25 +0000

    urlspatterns

--- budget_tracker/budget_tracker/urls.py
+++ budget_tracker/budget_tracker/urls.py
@@ -19,5 +19,8 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+path('admin/', admin.site.urls),
+path('accounts/', include('allauth.urls')), # Allauth authentication
+path('', include('main.urls')), # Main app URLs
 ]
+
